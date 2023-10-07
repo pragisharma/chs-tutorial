54f7aa57412d619270d76c224bfb3dad2778a85f
KGupta10 <imkavyagupta@gmail.com>
2023-10-06 21:51:12 -0700

    added contact us + about me

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 import os 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']  = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI']  = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI']  = 'postgresql://default:TiXOFWB9j4Kf@ep-late-cell-64247649.us-east-1.postgres.vercel-storage.com/verceldb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
 
 db = SQLAlchemy(app)
 
-class Teachers(db.Model):
+class Classes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(100), nullable=False)
-    lastname = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
+    teacher = db.Column(db.String(100), nullable=False)
+    #lastname = db.Column(db.String(100), nullable=False)
+    subject = db.Column(db.String(80), unique=True, nullable=False)
     room = db.Column(db.Integer)
     # i dont think we need this + it uses sq lite
     # created_at = db.Column(db.DateTime(timezone=True),
@@ -21,12 +21,58 @@ class Teachers(db.Model):
     status = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<Teacher {self.firstname}>'
+        return f'<tutorial {self.teacher}>'
 
 
-@app.route("/")
+#@app.route("/")
 def hello_world():
     return 'Hello World!'
+
+# ...
+
+@app.route('/')
+def index():
+    classes = Classes.query.all()
+    return render_template('index.html', classes=classes)
+
+@app.route('/aboutme/')
+def aboutme():
+    classes = Classes.query.all()
+    return render_template('aboutme.html', classes=classes)
+
+@app.route('/contactus/')
+def contactus():
+    classes = Classes.query.all()
+    return render_template('contactus.html', classes=classes)
+
+@app.route('/index/')
+def contactus():
+    classes = Classes.query.all()
+    return render_template('index.html', classes=classes)
+
+# ...
+
+
+@app.route('/<id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    teacher = Classes.query.get_or_404(id)
+
+    if request.method == 'POST':
+        #name = request.form['teacher']
+        status = request.form['status']
+        #room = request.form['room']
+
+        #teacher.name = name
+        teacher.status = status
+        #teacher.room = room
+
+        db.session.add(teacher)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', classes=teacher)
+
 
 if __name__ == "__main__":
     app.run()
